b6f5aea452a5369c82699ce90f093a2f96c084b0
teliauSimonTroalen <teliau.simontroalen@gmail.com>
2022-10-21 14:19:00 +0200

    Add Mort Script (dans mon level)

---
+++ Assets/Script/dieDieDieMyDarling.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dieDieDieMyDarling : MonoBehaviour
+{
+    public GameObject spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn.transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D truc)
+    {   Debug.Log("Touche");
+        if(truc.tag == "Kill")
+        {
+        Debug.Log("Meurt");
+        transform.position = spawn.transform.position;
+        }
+    }
+}
